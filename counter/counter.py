eac0e7f3b026d972bdaf99a9a66d76fb130d397c
Jacob Miske <jacobmiske@gmail.com>
2020-11-03 10:06:40 -0500

    errors based on inheirtance

--- counter/counter.py
+++ counter/counter.py
@@ -1,41 +1,59 @@
 # Jacob Miske MIT License
 import os
 import sys
-from PyQt5 import QtGui, QtCore
+import PyQt5
+from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtTest import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
 
-class MainWindow(QMainWindow):
+class MainWindow(QtWidgets.QMainWindow):
 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
+        self.count = 0
+
+        def counter_module():
+            group_box = QtWidgets.QGroupBox("Counter")
+            number = QtWidgets.QLCDNumber(3, self)
+            number.display(self.count)
+            button1 = QtWidgets.QPushButton("Increase by 1")
+            button1.clicked.connect(self.counter_up)
+            button2 = QtWidgets.QPushButton("Decrease by 1")
+            button2.clicked.connect(self.counter_down)
+            sublayout1 = QtWidgets.QVBoxLayout()
+            sublayout1.addWidget(button1)
+            sublayout1.addWidget(button2)
+            sublayout2 = QtWidgets.QVBoxLayout()
+            sublayout2.addWidget(number)
+            layout = QtWidgets.QHBoxLayout()
+            layout.addLayout(sublayout1, 1)
+            layout.addLayout(sublayout2, 2)
+            layout.addStretch(1)
+            group_box.setLayout(layout)
+            return group_box, number
 
         # Highest level layout object
-        layout = QVBoxLayout()
+        layout = QtWidgets.QVBoxLayout()
 
         # Sections of application
-        self.editor = QPlainTextEdit()
+        self.counting_module, self.count_LCD = counter_module()
 
-        # Setup the QTextEdit editor configuration
-        fixedfont = QFontDatabase.systemFont(QFontDatabase.FixedFont)
-        fixedfont.setPointSize(12)
-        self.editor.setFont(fixedfont)
 
         # self.path holds the path of the currently open file.
         # If none, we haven't got a file open yet (or creating new).
         self.path = None
 
         # Add widgets
-        layout.addWidget(self.editor)
+        layout.addWidget(self.counting_module)
 
-        container = QWidget()
+        container = QtWidgets.QWidget()
         container.setLayout(layout)
         self.setCentralWidget(container)
 
-        self.status = QStatusBar()
+        self.status = QtWidgets.QStatusBar()
         self.setStatusBar(self.status)
 
         self.update_title()
@@ -43,67 +61,37 @@ class MainWindow(QMainWindow):
         self.show()
 
         def create_file_toolbar():
-            file_toolbar = QToolBar("File")
-            file_toolbar.setIconSize(QSize(14, 14))
+            file_toolbar = QtWidgets.QToolBar("File")
+            file_toolbar.setIconSize(QtCore.QSize(14, 14))
             self.addToolBar(file_toolbar)
             file_menu = self.menuBar().addMenu("&File")
 
-            open_file_action = QAction(QIcon(os.path.join('images', 'question.png')), "Settings", self)
+            open_file_action = QtWidgets.QAction(QtGui.QIcon(os.path.join('images', 'question.png')), "Settings", self)
 
             open_file_action.setStatusTip("Settings")
             open_file_action.triggered.connect(self.file_open)
             file_menu.addAction(open_file_action)
             file_toolbar.addAction(open_file_action)
 
-            save_file_action = QAction(QIcon(os.path.join('images', 'disk.png')), "Save", self)
-            save_file_action.setStatusTip("Save current page")
-            save_file_action.triggered.connect(self.file_save)
-            file_menu.addAction(save_file_action)
-            file_toolbar.addAction(save_file_action)
-
-            saveas_file_action = QAction(QIcon(os.path.join('images', 'disk--pencil.png')), "Save As...", self)
-            saveas_file_action.setStatusTip("Save current page to specified file")
-            saveas_file_action.triggered.connect(self.file_saveas)
-            file_menu.addAction(saveas_file_action)
-            file_toolbar.addAction(saveas_file_action)
-
         create_file_toolbar()
 
+    def counter_up(self):
+        self.count += 1
+        self.count_LCD.display(self.count)
+
+    def counter_down(self):
+        self.count -= 1
+        self.count_LCD.display(self.count)
+
+
     def dialog_critical(self, s):
-        dlg = QMessageBox(self)
+        dlg = QtWidgets.QMessageBox(self)
         dlg.setText(s)
-        dlg.setIcon(QMessageBox.Critical)
+        dlg.setIcon(QtWidgets.QMessageBox.Critical)
         dlg.show()
 
     def file_open(self):
-        self.dialog_critical(s="See README.txt in ")
-
-    def file_save(self):
-        if self.path is None:
-            # If we do not have a path, we need to use Save As.
-            return self.file_saveas()
-        self._save_to_path(self.path)
-
-    def file_saveas(self):
-        path, _ = QFileDialog.getSaveFileName(self, "Save file", "", "Text documents (*.txt);All files (*.*)")
-        if not path:
-            # If dialog is cancelled, will return ''
-            return
-
-        self._save_to_path(path)
-
-    def _save_to_path(self, path):
-        text = self.editor.toPlainText()
-        try:
-            with open(path, 'w') as f:
-                f.write(text)
-
-        except Exception as e:
-            self.dialog_critical(str(e))
-
-        else:
-            self.path = path
-            self.update_title()
+        self.dialog_critical(s="See GitHub README")
 
     def update_title(self):
         self.setWindowTitle("Counter")
@@ -113,7 +101,7 @@ class MainWindow(QMainWindow):
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     app.setApplicationName("Counter")
     app_icon = QtGui.QIcon()
     # app_icon.addFile('arrow-continue.png')
